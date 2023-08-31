abf4b106cae33bb329e76aa9658b7cebde13c42a
Tmwakalasya <mwakalasyatuntu@gmail.com>
2023-08-31 11:28:17 -0400

    renamed a few of the output messages

--- src/PenniesConversion.java
+++ src/PenniesConversion.java
@@ -6,7 +6,7 @@ public class PenniesConversion {
 
     public static void main(String[] args){
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("How many pennies do you have in your piggybank ? ");
+        System.out.println("How many pennies do you have in your piggy bank ? ");
         pennies = keyboard.nextInt();
         getDollars();
         getQuarters();
@@ -16,23 +16,23 @@ public class PenniesConversion {
     public static void getDollars(){
         dollars = pennies/100;
         pennies = pennies % 100;
-        System.out.println("You have " + "$"+dollars + " in your piggybank");
+        System.out.println("You have " + "$"+dollars + " in your piggy bank");
         System.out.println("The remaining pennies are " + pennies);
     }
     public static  void getQuarters(){
         quarters = pennies / 25;
         pennies = pennies % 25;
-        System.out.println("You have " + quarters + " quarters " + " in your piggybank");
+        System.out.println("You have " + quarters + " quarters " + " in your piggy bank");
     }
     public static void getDimes(){
         dimes = pennies / 10;
         pennies = pennies % 10;
-        System.out.println("You have " + dimes + " dimes " + " in your piggybank");
+        System.out.println("You have " + dimes + " dimes " + " in your piggy bank");
 
     }
     public static  void getNickels(){
         nickels = pennies / 5;
         pennies = pennies % 5;
-        System.out.println("You have " + nickels + " nickels " + " in your piggybank");
+        System.out.println("You have " + nickels + " nickels " + " in your piggy bank");
     }
 }
